4a6c5d1398e5b5f02b655bcb2c425878c6355d5e
CBU <he01314906@gmail.com>
2024-10-23 15:46:30 +0800

    GitTest

---
+++ src/main/java/com/dobuy/patrick/Test.java
@@ -0,0 +1,21 @@
+package com.dobuy.patrick;
+
+public class Test {
+	private Integer num ;
+	private String name;
+public static void main(String[] args) {
+	
+}
+public Integer getNum() {
+	return num;
+}
+public void setNum(Integer num) {
+	this.num = num;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
